e31cd924d7456b97e4d8ab88cbbcc69c115cef42
Aiden Nguyen <nguhn1-27@rhodes.edu>
2026-02-08 21:30:21 -0600

    Submission for Lab 1

--- ping.c
+++ ping.c
@@ -1,6 +1,6 @@
 /*
  * ping.c - UDP ping/pong client code
- *          author: <your name>
+ *          author: Aiden Nguyen
  */
 #include <netdb.h>
 #include <stdio.h>
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
   char *pongport = strdup(PORTNO);      // default port
   int arraysize = 100;                  // default packet size
 
-  while ((ch = getopt(argc, argv, "h:n:p:")) != -1) {
+  while ((ch = getopt(argc, argv, "h:n:p:s:")) != -1) {
     switch (ch) {
     case 'h':
       ponghost = strdup(optarg);
@@ -42,9 +42,86 @@ int main(int argc, char **argv) {
     }
   }
 
+  // Create array
+  char *array = malloc(arraysize);
+  
+  if (!array) { 
+    perror("malloc"); 
+    exit(EXIT_FAILURE); 
+  }
+  
+  memset(array, 200, arraysize);
+  
+  // Get IP address, port number from getaddrinfo
+  struct addrinfo hints, *result;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET; // ipv4 addresses
+  hints.ai_socktype = SOCK_DGRAM; // udp
+
+  if (getaddrinfo(ponghost, pongport, &hints, &result) != 0) {
+    perror("getaddrinfo");
+    exit(EXIT_FAILURE);
+  }
+
+  // Call socket
+  int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+  if (sockfd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  // Start timer
+  double total_time = 0.0;
+  
+  // Ping packet loop
+  for (int i = 0; i < nping; i++) {
+    double start = get_wctime();
+  
+  // Send array to server
+    if (sendto(sockfd, array, arraysize, 0, result->ai_addr, result->ai_addrlen) < 0) {
+        perror("sendto"); 
+	exit(EXIT_FAILURE);
+    }
+
+  // Receive reply
+    char buf[1024];
+    int bytes = recvfrom(sockfd, buf, sizeof(buf), 0, NULL, NULL);
+    if (bytes < 0) { perror("recvfrom"); exit(EXIT_FAILURE); }
+
+    double end = get_wctime();
+
+    // Validate result
+    int error = 0;
+    for (int j = 0; j < arraysize; j++) {
+        if ((unsigned char)buf[j] != 201) { // 200 + 1
+            error = 1;
+            break;
+        }
+    }
+    if (error) errors++;
+
+    // Print
+    printf("ping[%d]: round-trip time: %.3f ms\n", i, (end - start) * 1000);
+    total_time += (end - start);
+    }
+
   // UDP ping implemenation goes here
-  printf("nping: %d arraysize: %d errors: %d ponghost: %s pongport: %s\n",
+  if (errors == 0)
+    printf("no errors detected\n");
+
+  printf("nping: %d arraysize: %d errors: %d ponghost: %s pongport: %s\n ",
       nping, arraysize, errors, ponghost, pongport);
+
+  printf("time to send %d packet of %d bytes %.3f ms (%.3f avg par packet)\n",
+      nping, arraysize, total_time * 1000, (total_time /nping) *1000);
+
+
+  free(array);
+  free(ponghost);
+  free(pongport);
+  freeaddrinfo(result);
+  close(sockfd);
 
   return 0;
 }
--- pong.c
+++ pong.c
@@ -1,6 +1,6 @@
 /*
  * pong.c - UDP ping/pong server code
- *          author: <your name>
+ *          author: Aiden Nguyen
  */
 #include <netdb.h>
 #include <stdio.h>
@@ -35,8 +35,62 @@ int main(int argc, char **argv) {
     }
   }
 
-  // pong implementation goes here.
+  // Resolve local address
+  struct addrinfo hints, *result;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if (getaddrinfo(NULL, pongport, &hints, &result) != 0) {
+      perror("getaddrinfo");
+      exit(EXIT_FAILURE);
+  }
+
+  // Get socket
+  int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+  if (sockfd < 0) { perror("socket"); exit(EXIT_FAILURE); }
+
+  // Bind
+  if (bind(sockfd, result->ai_addr, result->ai_addrlen) < 0) {
+    perror("bind"); exit(EXIT_FAILURE);
+  }
+
+  freeaddrinfo(result);
+
+  // Receive and respond
+  for (int i = 0; i < nping; i++) {
+    struct sockaddr_storage client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    char buf[1024];
+
+    int bytes = recvfrom(sockfd, buf, sizeof(buf), 0,
+               (struct sockaddr*)&client_addr, &addr_len);
+    if (bytes < 0) { perror("recvfrom"); exit(EXIT_FAILURE); }
+
+    // Convert client IP to string
+    char client_ip[INET_ADDRSTRLEN];
+    struct sockaddr_in *s = (struct sockaddr_in *)&client_addr;
+    inet_ntop(AF_INET, &s->sin_addr, client_ip, sizeof(client_ip));
+
+    // Print
+    printf("pong[%d]: received packet from %s\n", i, client_ip);
+
+    // Increment bytes
+    for (int j = 0; j < bytes; j++) {
+        buf[j] += 1;
+    }
+
+    if (sendto(sockfd, buf, bytes, 0,
+        (struct sockaddr*)&client_addr, addr_len) < 0) {
+       perror("sendto"); exit(EXIT_FAILURE);
+       }
+    }
+
   printf("nping: %d pongport: %s\n", nping, pongport);
+  
+  close(sockfd);
+  free(pongport);
 
   return 0;
 }
